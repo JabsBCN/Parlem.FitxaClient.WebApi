eb388a9dc6f20005f597048bb2bd971c75f3eaab
Xavier Garcia <javi.vbs@gmail.com>
2022-09-15 11:10:12 +0200

    rename file

---
+++ src/Parlem.FitxaClient.Api/FitxaClientController.cs
@@ -0,0 +1,18 @@
+namespace Parlem.FitxaClient.Api
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    public class FitxaClientController : ControllerBase
+    {
+        public FitxaClientController()
+        {
+
+        }
+
+        public object Get(object clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
