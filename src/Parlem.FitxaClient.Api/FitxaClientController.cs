564706e5624f105480a9b72a083f412658362286
Xavier Garcia <javi.vbs@gmail.com>
2022-09-15 13:02:08 +0200

    add decorators to controller

--- src/Parlem.FitxaClient.Api/FitxaClientController.cs
+++ src/Parlem.FitxaClient.Api/FitxaClientController.cs
@@ -2,7 +2,9 @@ namespace Parlem.FitxaClient.Api
 {
     using Application.Models;
     using Microsoft.AspNetCore.Mvc;
+    using System.Net;
 
+    [Route("api/[controller]/[action]")]
     public class FitxaClientController : ControllerBase
     {
         public FitxaClientController()
@@ -10,6 +12,9 @@ namespace Parlem.FitxaClient.Api
 
         }
 
+        [Route("client/{clientId}")]
+        [HttpGet(Name = "GetFitxaClient")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         public ActionResult<ClientDto> Get(long clientId)
         {
             return Ok(new ClientDto(555555, "nif", "11223344E", "it@parlem.com", 11111, "Enriqueta", "Parlem", 668668668));
