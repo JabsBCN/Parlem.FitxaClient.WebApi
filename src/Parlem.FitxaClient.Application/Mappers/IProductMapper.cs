89acafecb01c6b5f46828fb9c08376e603ddc330
Xavier Garcia <javi.vbs@gmail.com>
2022-09-16 13:18:45 +0200

    add product mapper

---
+++ src/Parlem.FitxaClient.Application/Mappers/IProductMapper.cs
@@ -0,0 +1,10 @@
+﻿using Parlem.FitxaClient.Application.Models;
+using Parlem.FitxaClient.Domain.Models;
+
+namespace Parlem.FitxaClient.Application.Mappers
+{
+    public interface IProductMapper
+    {
+        ProductDto MapFrom(Product customer);
+    }
+}
---
+++ src/Parlem.FitxaClient.Application/Mappers/ProductMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Parlem.FitxaClient.Application.Mappers
+{
+    using Parlem.FitxaClient.Domain.Models;
+    using Parlem.FitxaClient.Application.Models;
+
+    public class ProductMapper : IProductMapper
+    {
+        public ProductDto MapFrom(Product product) {
+            return new ProductDto(product.Id, product.ProductName, product.ProductTypeName, product.NumeracioTerminal, product.SoldAt, product.CustomerId);
+        }
+    }
+}
