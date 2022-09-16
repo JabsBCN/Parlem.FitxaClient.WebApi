7b7c6b2db0f6e9f92dd83c7e894472608b1ac811
Xavier Garcia <javi.vbs@gmail.com>
2022-09-16 13:19:06 +0200

    add product repository

---
+++ src/Parlem.FitxaClient.Infrastructure/Contracts/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Parlem.FitxaClient.Domain.Models;
+
+namespace Parlem.FitxaClient.Infrastructure.Contracts
+{
+    public  interface IProductRepository
+    {
+        Product GetCustomerProductsByCustomerId(long customerId);
+    }
+}
---
+++ src/Parlem.FitxaClient.Infrastructure/Implementations/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Parlem.FitxaClient.Domain.Models;
+using Parlem.FitxaClient.Infrastructure.Contracts;
+
+namespace Parlem.FitxaClient.Infrastructure.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Product GetCustomerProductsByCustomerId(long customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
