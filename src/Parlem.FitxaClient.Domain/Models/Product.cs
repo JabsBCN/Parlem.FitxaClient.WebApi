19755c098249e375a32a670df0dc10857aa76072
Xavier Garcia <javi.vbs@gmail.com>
2022-09-16 13:19:48 +0200

    add product entity

---
+++ src/Parlem.FitxaClient.Domain/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Parlem.FitxaClient.Domain.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("_id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; }
+
+        [JsonPropertyName("productTypeName")]
+        public string ProductTypeName { get; set; }
+
+        [JsonPropertyName("numeracioTerminal")]
+        public long NumeracioTerminal { get; set; }
+
+        [JsonPropertyName("soldAt")]
+        public DateTime SoldAt { get; set; }
+
+        [JsonPropertyName("customerId")]
+        public long CustomerId { get; set; }
+
+
+        public Product()
+        {
+        }
+
+        internal Product(long id, string productName, string docNum, string numeracioTerminal, DateTime soldAt, long customerId)
+        {
+            Id = id;
+            ProductName = productName;
+            ProductTypeName = docNum;
+            NumeracioTerminal = numeracioTerminal;
+            SoldAt = soldAt;
+            CustomerId = customerId;
+        }
+    }
+}
